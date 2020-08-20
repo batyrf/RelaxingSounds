f9bcb66ec0b2e24aa951f5b92c7d71c1bf37a1f8
batyrff <mlale420@gmail.com>
2020-08-21 00:20:45 +0300

    added screens

---
+++ app/src/main/java/tm/mr/relaxingsounds/ui/library/view/LibraryFragment.kt
@@ -0,0 +1,22 @@
+package tm.mr.relaxingsounds.ui.library.view
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.fragment_library.*
+import tm.mr.relaxingsounds.R
+
+class LibraryFragment : Fragment(R.layout.fragment_library) {
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btn.setOnClickListener {
+            val action = LibraryFragmentDirections.actionLibraryFragmentToListFragment()
+            it.findNavController().navigate(action)
+        }
+    }
+
+}
