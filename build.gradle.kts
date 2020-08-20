89218d34cc0a780fd9625f2a13d42f604a75277a
batyrff <mlale420@gmail.com>
2020-08-21 00:44:07 +0300

    adding hilt dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,7 @@ buildscript {
         classpath("com.android.tools.build:gradle:4.0.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:${Versions.nav_version}")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:${Versions.hilt}")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
