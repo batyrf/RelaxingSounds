8c4e1818dcfa61f2d78502c923a622460c94a65f
batyrff <mlale420@gmail.com>
2020-08-22 23:00:23 +0300

    fixed 'error: You must annotate primary keys with @NonNull. "id" is nullable. SQLite considers this a bug and Room does not allow it.'

--- app/src/main/java/tm/mr/relaxingsounds/data/model/Sound.kt
+++ app/src/main/java/tm/mr/relaxingsounds/data/model/Sound.kt
@@ -1,14 +1,15 @@
 package tm.mr.relaxingsounds.data.model
 
+import androidx.annotation.NonNull
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "Sounds")
 data class Sound(
-    @PrimaryKey val id: String?,
+    @NonNull @PrimaryKey val id: String,
     @ColumnInfo(name = "title") val title: String?,
     @ColumnInfo(name = "url") val url: String?,
     @ColumnInfo(name = "isLike") val isLike: Boolean?,
-    @ColumnInfo(name = "categoryId") val categoryId: String?
+    @NonNull @ColumnInfo(name = "categoryId") val categoryId: String
 )
